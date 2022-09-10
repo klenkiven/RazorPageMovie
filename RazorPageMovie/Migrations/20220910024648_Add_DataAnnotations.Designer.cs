ac89015af364fe35177c00353eaa12924b1844c3
KlenKiven <wzl709@outlook.com>
2022-09-10 10:49:20 +0800

    MIGRATION 更新实体类并且迁移数据库

--- RazorPageMovie/Migrations/20220910024648_Add_DataAnnotations.Designer.cs
+++ RazorPageMovie/Migrations/20220910024648_Add_DataAnnotations.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorPageMovie.Data;
 
@@ -10,9 +11,10 @@ using RazorPageMovie.Data;
 namespace RazorPageMovie.Migrations
 {
     [DbContext(typeof(RazorPageMovieContext))]
-    partial class RazorPageMovieContextModelSnapshot : ModelSnapshot
+    [Migration("20220910024648_Add_DataAnnotations")]
+    partial class Add_DataAnnotations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.8");
---
+++ RazorPageMovie/Migrations/20220910024648_Add_DataAnnotations.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RazorPageMovie.Migrations
+{
+    public partial class Add_DataAnnotations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
