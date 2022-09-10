75f311d8d44f914068102ebc34272ef55349f03c
KlenKiven <wzl709@outlook.com>
2022-09-10 10:48:56 +0800

    Add Movie实体类数据校验以及一些注释说明

--- RazorPageMovie/Models/Movie.cs
+++ RazorPageMovie/Models/Movie.cs
@@ -12,16 +12,19 @@ public class Movie
     // C# 中可以在实体类中添加一个 字段（field）
     public int ID { get; set; }
     
-    // 对于字段而言，可以设置它的默认值
+    // 对于字段而言，可以设置它的默认值 下面的两种方式都是有效的
+    //[StringLength(60, MinimumLength = 3)]
+    //[Required]
     [StringLength(60, MinimumLength = 3), Required]
     public string Title { get; set; } = string.Empty;
     
     // 这里使用到了 注解（Attribute） 标注这个字段的类型
-    [Display(Name = "Release Date")]
+    [Display(Name = "Release Date"), DataType(DataType.Date)]
     // 展示格式在这里设置
-    [DisplayFormat(DataFormatString = "0:yyyy-MM-dd", ApplyFormatInEditMode = true)]
-    // 关于日期的数据校验，需要使用Range来校验参数
-    [Range(typeof(DateTime), "1/1/1966", "1/1/2020"), DataType(DataType.Date)]
+    // [DisplayFormat(DataFormatString = "0:yyyy-MM-dd", ApplyFormatInEditMode = true)]
+    // 关于日期的数据校验，需要使用Range来校验参数，但是不建议这样使用
+    // see https://docs.microsoft.com/en-us/aspnet/core/tutorials/razor-pages/validation?view=aspnetcore-6.0&tabs=visual-studio-code#use-datatype-attributes
+    // [Range(typeof(DateTime), "1/1/1966", "1/1/2020")]
     public DateTime ReleaseTime { get; set; }
     
     // 类型
@@ -30,15 +33,12 @@ public class Movie
     
     // 价格
     // 这里使用到了一个比较特别的类型 decimal
-    [Range(1, 100)]
-    [DataType(DataType.Currency)]
+    [Range(1, 100), DataType(DataType.Currency)]
     [Column(TypeName = "decimal(18,2)")]
     public decimal Price { get; set; }
     
     // 排名
-    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
-    [StringLength(5)]
-    [Required]
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5)]
     public string Rating { get; set; } = string.Empty;
     
 }
